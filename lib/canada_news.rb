6749ebb686d6761e508a8b511d23d5e19ca735f5
bryanhou1 <Hou@wpa002232.wireless.mcgill.ca>
2017-04-13 13:45:08 -0400

    added to gem CLI structure

--- lib/canada_news.rb
+++ lib/canada_news.rb
@@ -1,5 +1,48 @@
-require "canada_news/version"
+require_relative "canada_news/version"
 
 module CanadaNews
-  # Your code goes here...
+  class CLI
+  	def play
+  		greeting
+
+  		#implement loop that can be exited with user input and enter previous level
+  		display_trending
+  		options
+  		display_article
+  	end
+
+  	def greeting
+  
+  	end
+
+  	def display_trending
+  		puts <<-DOC
+1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+DOC
+  	end
+
+  	def options
+  		input = gets.strip
+  		#use input to scrap from article website
+
+  	end
+  	
+
+  	def display_article
+  		puts "article place holder"
+  	end
+
+
+  	def scrapping
+  	end
+  end
 end
