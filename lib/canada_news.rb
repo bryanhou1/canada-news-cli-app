140a74c7fd1da95b5d738e41034711985da3f63b
bryanhou1 <Hou@Bryans-MacBook-Pro.local>
2017-04-13 16:48:35 -0400

    refined menu logic, still erroneous

--- lib/canada_news.rb
+++ lib/canada_news.rb
@@ -4,20 +4,33 @@ module CanadaNews
   class CLI
   	def play
   		greeting
-
-  		#implement loop that can be exited with user input and enter previous level
-  		display_trending
-  		options
-  		display_article
+  		input = nil
+      body
+      exit_greeting
   	end
 
   	def greeting
-  
+  		puts "Welcome back!"
+  		puts "Getting your daily canadian news..."
+  		sleep 0.5
   	end
+
+  	def body
+      display_trending
+      input = article_options
+      while input != "exit"
+        if input.match(/^([1-9]|10)/)
+          display_article(input)
+        end
+        input = post_article_options
+        self.body if input == 'up'
+      end
+    end
 
   	def display_trending
   		puts <<-DOC
-1.
+Today's trending news:
+1. 
 2.
 3.
 4.
@@ -30,17 +43,35 @@ module CanadaNews
 DOC
   	end
 
-  	def options
-  		input = gets.strip
-  		#use input to scrap from article website
-
+  	def article_options #returns 1-10 or exit
+        puts "Enter 1 - 10 to read more about the news, or 'exit' to quit"
+        input = gets.strip
+        if !input.match(/^([1-9]|10|exit)$/)
+          puts "Option does not exist, please retry. \n\n\n\n"
+          article_options
+        end
+      input
   	end
   	
-
-  	def display_article
-  		puts "article place holder"
+  	def post_article_options
+  		puts "Enter 1 - 10 to read another news article, 'up' to go up to see the trending news again or 'exit' to quit"
+      input = gets.strip
+      if !input.match(/^([1-9]|10|up|exit)$/)
+        puts "Option does not exist, please retry. \n\n\n\n"
+        post_article_options
+      end
+      input
   	end
 
+  
+  	def display_article(options)
+  		puts "Article #{options}"
+  		puts "<article place holder>"
+  	end
+
+  	def exit_greeting
+      puts "Bye."
+    end
 
   	def scrapping
   	end
