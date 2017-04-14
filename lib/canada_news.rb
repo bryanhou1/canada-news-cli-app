501acc56d0241311e3e0b78981325401f12a4cd3
bryanhou1 <Hou@wpa002021.wireless.mcgill.ca>
2017-04-14 13:57:04 -0400

    added real trending article titles and internalize scraping

--- lib/canada_news.rb
+++ lib/canada_news.rb
@@ -4,6 +4,7 @@ module CanadaNews
   class CLI
   	def play
   		greeting
+  		Article.scrape
   		input = nil
       body
       exit_greeting
@@ -12,7 +13,6 @@ module CanadaNews
   	def greeting
   		puts "Welcome back!"
   		puts "Getting your daily canadian news..."
-  		sleep 0.5
   	end
 
   	def body
@@ -31,23 +31,24 @@ module CanadaNews
     end
 
   	def display_trending
-  		puts <<-DOC
-Today's trending news:
-1. 
-2.
-3.
-4.
-5.
-6.
-7.
-8.
-9.
-10.
-DOC
+  		puts <<~DOC
+				Today's trending news:
+				1. #{Article.all[0].attributes[:title]}
+				2. #{Article.all[1].attributes[:title]}
+				3. #{Article.all[2].attributes[:title]}
+				4. #{Article.all[3].attributes[:title]}
+				5. #{Article.all[4].attributes[:title]}
+				6. #{Article.all[5].attributes[:title]}
+				7. #{Article.all[6].attributes[:title]}
+				8. #{Article.all[7].attributes[:title]}
+				9. #{Article.all[8].attributes[:title]}
+				10. #{Article.all[9].attributes[:title]}
+
+			DOC
   	end
 
   	def article_options #returns 1-10 or exit
-        puts "Enter 1 - 10 to read more about the news, or 'exit' to quit"
+        print "Enter 1 - 10 to read more about the news, or 'exit' to quit: "
         input = gets.strip
         if !input.match(/^([1-9]|10|exit)$/)
           puts "Option does not exist, please retry. \n\n\n\n"
@@ -57,7 +58,7 @@ DOC
   	end
   	
   	def post_article_options
-  		puts "Enter 1 - 10 to read another news article, 'up' to go up to see the trending news again or 'exit' to quit"
+  		print "Enter 1 - 10 to read another news article, 'up' to go up to see the trending news again or 'exit' to quit: "
       input = gets.strip
       if !input.match(/^([1-9]|10|up|exit)$/)
         puts "Option does not exist, please retry. \n\n\n\n"
@@ -69,7 +70,7 @@ DOC
   	def display_article(options)
   		puts "Article #{options}"
   		article = Article.all[options.to_i-1]
-  		puts article.attributes[:title]
+  		puts article.attributes[:title].red
   		puts article.attributes[:author]
   		puts article.attributes[:time_posted]
   		puts article.attributes[:content]
@@ -106,14 +107,14 @@ DOC
 					url: "http://www.cbc.ca#{article.attribute("href").value}"
 					})
   		}
-@@con=1
+# @@con=1
   		Article.all.each{ |article|
   			
   			node = Nokogiri::HTML(open(article.attributes[:url]))
-  			binding.pry if @@con = 1
-  			# article.attributes[:author] = node.css(".small .spaced").text
-  			# article.attributes[:time_posted] = node.css(".delimited").text
-  			# article.attributes[:content] = node.css(".story-content").text
+  			# binding.pry if @@con == 1
+  			article.attributes[:author] = node.css(".small .spaced").text
+  			article.attributes[:time_posted] = node.css(".delimited").text
+  			article.attributes[:content] = node.css(".story-content").text
   		}
   		# individual_articles = [Nokogiri::HTML(open("http://www.cbc.ca/news/trending"))]
   	end
